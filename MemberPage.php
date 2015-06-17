923351435fedffd3d08f0046de767dda2ec01ea1
natchar <natchar.r@alumni.ubc.ca>
2015-06-17 03:13:10 -0700

    Members now see points and what they got from datablazers

--- MemberPage.php
+++ MemberPage.php
@@ -1,6 +1,91 @@
 <html>
 
 <body>
+      <?php
+      $success = True; //keep track of errors so it redirects the page only if there are no errors
+      $db_conn = oci_connect("ora_r3v8", "a21491139", "ug");
+
+      function executePlainSQL($cmdstr) { //takes a plain (no bound variables) SQL command and executes it
+        //echo "<br>running ".$cmdstr."<br>";
+        global $db_conn, $success;
+        $statement = OCIParse($db_conn, $cmdstr); //There is a set of comments at the end of the file that describe some of the OCI specific functions and how they work
+
+        if (!$statement) {
+          echo "<br>Cannot parse the following command: " . $cmdstr . "<br>";
+          $e = OCI_Error($db_conn); // For OCIParse errors pass the       
+          // connection handle
+          echo htmlentities($e['message']);
+          $success = False;
+        }
+
+        $r = OCIExecute($statement, OCI_DEFAULT);
+        if (!$r) {
+          echo "<br>Cannot execute the following command: " . $cmdstr . "<br>";
+          $e = oci_error($statement); // For OCIExecute errors pass the statementhandle
+          echo htmlentities($e['message']);
+          $success = False;
+        } else {
+
+        }
+        return $statement;
+
+      }
+      
+      function executeBoundSQL($cmdstr, $list) {
+        /* Sometimes a same statement will be excuted for severl times, only
+         the value of variables need to be changed.
+         In this case you don't need to create the statement several times; 
+         using bind variables can make the statement be shared and just 
+         parsed once. This is also very useful in protecting against SQL injection. See example code below for       how this functions is used */
+
+        global $db_conn, $success;
+        $statement = OCIParse($db_conn, $cmdstr);
+
+        if (!$statement) {
+          echo "<br>Cannot parse the following command: " . $cmdstr . "<br>";
+          $e = OCI_Error($db_conn);
+          echo htmlentities($e['message']);
+          $success = False;
+        }
+
+        foreach ($list as $tuple) {
+          foreach ($tuple as $bind => $val) {
+            //echo $val;
+            //echo "<br>".$bind."<br>";
+            OCIBindByName($statement, $bind, $val);
+            unset ($val); //make sure you do not remove this. Otherwise $val will remain in an array object wrapper which will not be recognized by Oracle as a proper datatype
+
+          }
+          $r = OCIExecute($statement, OCI_DEFAULT);
+          if (!$r) {
+            echo "<br>Cannot execute the following command: " . $cmdstr . "<br>";
+            $e = OCI_Error($statement); // For OCIExecute errors pass the statementhandle
+            echo htmlentities($e['message']);
+            echo "<br>";
+            $success = False;
+          }
+        }
+
+      }
+
+      function printResult($result) { //prints results from a select statement
+        echo "<br>Got data from table tab1:<br>";
+        echo "<table>";
+        echo "<tr><th>ID</th><th>Name</th></tr>";
+
+        while ($row = OCI_Fetch_Array($result, OCI_BOTH)) {
+          echo "<tr><td>" . $row["NID"] . "</td><td>" . $row["NAME"] . "</td></tr>"; //or just use "echo $row[0]" 
+        }
+        echo "</table>";
+
+      }
+
+
+
+      ?>
+
+
+
       <div class="row">
         <div class="large-3 columns">
           <h1><img src="http://placehold.it/400x100&text=DATABLAZERGAMES"/></h1>
@@ -41,7 +126,8 @@
             $db_conn = oci_connect("ora_r3v8", "a21491139", "ug");
             //!!! NEED TO TAKE MEMBER ID FROM LOGIN
             $Memberid = 90876543;
-            $Memberpoints = oci_parse($db_conn, "SELECT POINTS FROM MEMBER MEMBER WHERE CID=:Memberid");
+            $Memberpoints = oci_parse($db_conn, "SELECT POINTS FROM MEMBER WHERE CID=:Memberid");
+
             oci_bind_by_name($Memberpoints, ":Memberid", $Memberid);
             $Memberpointsexecute = oci_execute($Memberpoints);
             echo "<p> MemberID: " . $Memberid . "</p>";
@@ -50,27 +136,48 @@
                 print "<p> You have " . $row['POINTS'] . " points. </p>";
               }
             }
-
-            //while ($row = oci_fetch_array($Memberpointsexecute, OCI_BOTH)) {
-            //  echo "<p> you have " . $row['POINTS'] . " points </p>";
-            //}
-
           
-            //echo "you can afford the following items!";
+            $MemberAffords = oci_parse($db_conn, "SELECT PB.NAME, PB.PRICE FROM MEMBER M, PRODUCTBARCODE PB WHERE M.POINTS > PB.PRICE AND M.CID = :Memberid ORDER BY PRICE");
+            oci_bind_by_name($MemberAffords, ":Memberid", $Memberid);
+            $MemberAffordsExecute = oci_execute($MemberAffords);
 
+            echo "<p> You can afford the following items! </p>";
+
+            if($MemberAffordsExecute){
+              while ($row = oci_fetch_array($MemberAffords)){
+                echo  $row[1]." points: ".$row[0] . "<br>";
+              }
+            }
           ?>
         </div>
         
+
         <div class="large-4 columns">
           <img src="http://placehold.it/400x300&text=[img]"/>
-          <h4>Your history</h4>
-          <p>Bacon ipsum dolor sit amet nulla ham qui sint exercitation eiusmod commodo, chuck duis velit. Aute in reprehenderit, dolore aliqua non est magna in labore pig pork biltong. Eiusmod swine spare ribs reprehenderit culpa. Boudin aliqua adipisicing rump corned beef.</p>
+          <h4>What you got from us:</h4>
+
+          <?php
+            $MemberHistory = oci_parse($db_conn, "SELECT PB.NAME, SUM(PT.QUANTITY) FROM MEMBER M, PURCHASETRACKS PT, PRODUCTBARCODE PB WHERE M.CID=PT.CID AND PT.BARCODE=PB.BARCODE AND M.CID = :Memberid GROUP BY PB.NAME");
+
+            oci_bind_by_name($MemberHistory, ":Memberid", $Memberid);
+            $MemberHistoryExecute = oci_execute($MemberHistory);
+
+            if($MemberHistoryExecute){
+              while($row = oci_fetch_array($MemberHistory)){
+                echo $row[1]. " " . $row[0] . "<br>";
+              }
+            }
+
+          ?>
+          <p> </p>
+
+
         </div>
         
         <div class="large-4 columns">
           <img src="http://placehold.it/400x300&text=[img]"/>
           <h4>This is a content section.</h4>
-          <p>Bacon ipsum dolor sit amet nulla ham qui sint exercitation eiusmod commodo, chuck duis velit. Aute in reprehenderit, dolore aliqua non est magna in labore pig pork biltong. Eiusmod swine spare ribs reprehenderit culpa. Boudin aliqua adipisicing rump corned beef.</p>
+          <p> </p>
         </div>
       
         </div>
@@ -168,6 +275,7 @@ echo "</select>";
           </div>
         </div> 
       </footer>
+
     </body>
 
 </html>
